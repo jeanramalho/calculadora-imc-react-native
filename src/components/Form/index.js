5cbfe3845a677167f326b75841d318a8bad617e2
jeanramalho <jeanramalho.dev@gmail.com>
2023-02-08 12:51:55 -0300

    Cria estados para gerenciar componentes

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,14 @@
-import React from "react"
+import React, {userState} from "react"
 import { View, Text, TextInput, Button} from "react-native"
+import ResultImc from "./ResultImc/"
 
 export default function Form() {
+
+const {height, setHeight } = useState(null)
+const {Weight, setWeight} = useState(null)
+const { messageImc, setMessageImc} = useState("Preencha o peso e altura")
+const {imc, setImc} = useState(null)
+const {textButtn, setTextButton} = useState(null)
     
     return(
         <View>
