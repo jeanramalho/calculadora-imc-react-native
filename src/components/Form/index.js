d8fb4adca318aef37deba81ff6853d125b1b0f1c
jeanramalho <jeanramalho.dev@gmail.com>
2023-02-08 12:54:39 -0300

    Utiliza função para criar calculo de imc

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,4 +1,4 @@
-import React, {userState} from "react"
+import React, {useState} from "react"
 import { View, Text, TextInput, Button} from "react-native"
 import ResultImc from "./ResultImc/"
 
@@ -8,7 +8,11 @@ const {height, setHeight } = useState(null)
 const {Weight, setWeight} = useState(null)
 const { messageImc, setMessageImc} = useState("Preencha o peso e altura")
 const {imc, setImc} = useState(null)
-const {textButtn, setTextButton} = useState(null)
+const {textButtn, setTextButton} = useState("Calcular")
+
+function imcCalculator(){
+    return setImc((Weight/(height*height)).toFixed(2))
+}
     
     return(
         <View>
