6f960125c9c6b790f1880c208d88993fd0bea041
jeanramalho <jeanramalho.dev@gmail.com>
2023-02-10 09:58:12 -0300

    Começa ajustar estilo do form

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { View, Text} from "react-native"
+import Styles from "./style"
 
 export default function Title() {
     
     return(
-        <View>
-            <Text>CALCULADORA IMC</Text>
+        <View style={Styles.boxTitle}>
+            <Text style={Styles.textTitle}>CALCULADORA IMC</Text>
         </View>
     )
 }
